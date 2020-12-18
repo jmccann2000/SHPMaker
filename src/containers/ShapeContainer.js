30b95d5e1c942a04eaa1aeefac51525ca0cd82f8
Liam C <liamc628@gmail.com>
2020-12-18 16:19:32 -0500

    styling

--- src/containers/ShapeContainer.js
+++ src/containers/ShapeContainer.js
@@ -1,10 +1,19 @@
 import React from 'react';
+import styles from './../styles.modules.css'
 
 export class ShapeContainer extends React.Component{
   render(){
+
     return (
       <div className='shapeWrapper'>
-        <p>{this.props.shape}</p>
+        <div className = 'shapeButton'>
+          <button> {this.props.shape} </button>
+        </div>
+        <div className = 'smallButton'>
+          <button>X</button>
+          <button>U</button>
+          <button>D</button>
+        </div>
       </div>
     )
   }
